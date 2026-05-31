5f6e78e3b7fc5254b6d96e8a24ca88e5a30788d3
Ramon Viedma <ramon.viedma@hp.com>
2026-05-31 19:25:38 +0200

    Python modules to start building app

---
+++ object_detector.py
@@ -0,0 +1,83 @@
+from pathlib import Path
+from ultralytics import YOLO
+
+# Paths
+DEFAULT_MODEL_PATH = "sku110k_768_e20_pat5.pt"
+
+# Inference parameters
+IMG_SIZE = 768          # use the same size you trained with, if possible
+# If you see too many false positives, increase CONF_THRES.
+CONF_THRES = 0.25
+# If neighboring products are being suppressed, increase IOU_THRES slightly, for example to 0.70.
+IOU_THRES = 0.60
+# shelves can contain many products
+MAX_DET = 100
+
+
+def filter_border_boxes(boxes, img_width, img_height, border = 20):
+    """Return only boxes that do not touch the image border."""
+    filtered = []
+    for box in boxes:
+        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+        if x1 <= border or y1 <= border or x2 >= img_width - border or y2 >= img_height - border:
+            continue
+        filtered.append(box)
+    return filtered
+
+class ObjectDetector:
+    def __init__(self, model_path: str = DEFAULT_MODEL_PATH):
+        self.model = YOLO(model_path)
+
+    def _detect(self, source, enable_filter: bool) -> tuple[list, any]:
+        results = self.model.predict(
+            source=source,
+            imgsz=IMG_SIZE,
+            conf=CONF_THRES,
+            iou=IOU_THRES,
+            max_det=MAX_DET,
+            save=False,
+            verbose=True,
+        )
+
+        # results is a list; for one image, take results[0]
+        result = results[0]
+
+        # Print detection summary
+        num_boxes = len(result.boxes)
+        print(f"Detected products: {num_boxes}")
+
+        if enable_filter:
+            result.boxes = filter_border_boxes(result.boxes, img_width=result.orig_img.shape[1], img_height=result.orig_img.shape[0])
+            print(f"Detected products (after filtering border boxes): {len(result.boxes)}")
+
+        # Draw boxes on the image
+        annotated = result.plot()
+
+        return result.boxes, annotated
+
+    def detect_from_file(self, image_file: str, enable_filter: bool = True) -> tuple[list, any]:
+        image_path = Path(image_file)
+
+        if not image_path.exists():
+            raise FileNotFoundError(f"Image file not found: {image_path}")
+
+        return self._detect(str(image_path), enable_filter=enable_filter)
+
+    def detect_from_frame(self, frame, enable_filter: bool = True) -> tuple[list, any]:
+        return self._detect(frame, enable_filter=enable_filter)
+
+
+    def print_boxes(self, boxes):
+        # Print each box
+        for i, box in enumerate(boxes):
+            xyxy = box.xyxy[0].cpu().numpy()
+            conf = float(box.conf[0].cpu().numpy())
+            cls_id = int(box.cls[0].cpu().numpy())
+
+            x1, y1, x2, y2 = xyxy
+            print(
+                f"Box {i + 1}: "
+                f"class={cls_id}, "
+                f"conf={conf:.3f}, "
+                f"x1={x1:.1f}, y1={y1:.1f}, x2={x2:.1f}, y2={y2:.1f}"
+            )
---
+++ project.py
@@ -0,0 +1,150 @@
+from pathlib import Path
+import argparse
+from object_detector import ObjectDetector
+import cv2
+from hand_detector import HandDetector
+
+def process_image(image_path: str, save: bool = False) -> int:
+    detector = ObjectDetector()
+    boxes, annotated_image = detector.detect_from_file(image_path)
+
+    if save:
+        # Save annotated image
+        output_path = Path(f"{Path(image_path).stem}_pred.jpg")
+        cv2.imwrite(str(output_path), annotated_image)
+        print(f"Saved annotated image to: {output_path}")
+
+    return 0
+
+def process_video(video_path: str, save: bool = False) -> int:
+    detector = ObjectDetector()
+    hand_detector = HandDetector(mode=HandDetector.Mode.VIDEO)
+
+    # Open video file
+    cap = cv2.VideoCapture(video_path)
+    
+    if not cap.isOpened():
+        raise ValueError(f"Cannot open video file: {video_path}")
+    
+    # Get video properties
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    # Define video writer
+    out = None
+    if save:
+        output_path = Path(f"{Path(video_path).stem}_pred.mp4")
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        out = cv2.VideoWriter(str(output_path), fourcc, fps, (width, height))
+    
+    print(f"Video: {Path(video_path).name}")
+    print(f"FPS: {fps}, Resolution: {width}x{height}, Total frames: {total_frames}")
+    print("Processing video...")
+    print("Press 'q' to close the preview window.")
+
+    cv2.namedWindow("Annotated Video", cv2.WINDOW_NORMAL)
+    
+    frame_count = 0
+    
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            break
+        
+        frame_count += 1
+        
+        # Run inference on frame
+        #boxes, annotated_frame = detector.detect_from_frame(frame)
+        found, start_point, direction, hand_annotated_frame = hand_detector.detect(frame, timestamp_ms=int(cap.get(cv2.CAP_PROP_POS_MSEC)))
+        
+        if found and hand_annotated_frame is not None:
+            annotated_frame = hand_annotated_frame
+            cv2.imshow("Annotated Video", annotated_frame)
+        else:
+            cv2.imshow("Annotated Video", frame)
+        
+        # Show the frame
+        #cv2.imshow("Annotated Video", annotated_frame)
+
+        # Process GUI events so the window updates and can receive key presses
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            print("Preview stopped by user.")
+            break
+        
+        # Write frame to output video
+        if save and out:
+            out.write(annotated_frame)
+        
+        # Print progress
+        if frame_count % 30 == 0:
+            print(f"Processed frame {frame_count}/{total_frames} ({frame_count/total_frames*100:.1f}%)")
+    
+    # Release resources
+    cap.release()
+    if out:
+        out.release()
+    cv2.destroyAllWindows()
+
+    return 0
+
+def process_webcam():
+    print("Webcam processing is not implemented yet.")
+    return 0
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Run YOLO inference on one shelf image."
+    )
+
+    parser.add_argument(
+        "--image",
+        required=False,
+        help="Path to input image, for example /path/to/IMG_3189.jpg",
+    )
+
+    parser.add_argument(
+        "--video",
+        required=False,
+        help="Path to input video, for example /path/to/video.mp4",
+    )
+
+    parser.add_argument(
+        "--webcam",
+        required=False,
+        help="Use webcam for real-time inference.",
+    )
+
+    parser.add_argument(
+        "--save",
+        action="store_true",
+        help="Save the annotated image to disk.",
+    )
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    counter = sum([bool(args.image), bool(args.video), bool(args.webcam)])
+    if counter > 1:
+        print("Error: Please provide only one of --image, --video, or --webcam.")
+        exit(1)
+    
+    if counter == 0:
+        print("Error: Please provide one of --image, --video, or --webcam to process.")
+        exit(1)
+
+    if args.image:
+        exit(process_image(args.image, save=args.save))
+
+    if args.video:
+        exit(process_video(args.video, save=args.save))
+
+    if args.webcam:
+        exit(process_webcam())
+
+if __name__ == "__main__":
+    main()
